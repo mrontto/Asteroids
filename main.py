935d7203c0295059aeaa1f910065fdd75c123c66
mrontto <mrontto@gmail.com>
2024-11-23 15:34:21 -0500

    Added Player and Player movement, updated main to use draw and update groupings.

--- main.py
+++ main.py
@@ -3,21 +3,50 @@
 # throughout this file
 import pygame # type: ignore
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill((0,0,0))
+        for update in updatable:
+            update.update(dt)
+
+        screen.fill((0,0,0))    
+
+        for objects in drawable:
+            objects.draw(screen)
+
+
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+
 
 
 
